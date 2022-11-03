1bf592fcf52ce2cc074b7d60d22b2500ba712661
kamapu <kamapu78@gmail.com>
2022-11-03 16:32:00 +0100

    Commit from git2r

--- _build_site.R
+++ _build_site.R
@@ -5,19 +5,28 @@
 
 library(blogdown)
 library(zip)
+library("git2r")
 
 # Produce data set
 Files <- list.files("../../vhs-bonn/bonn-2022-r-intro/data/", full.names = TRUE)
+Files[!grepl(".log", Files, fixed = TRUE)]
 unlink("static/documents/KursDateien.zip")
 zip("static/documents/KursDateien.zip", Files, mode = "cherry-pick")
 
+# Build the page
 build_site(build_rmd = TRUE)
-serve_site()
-stop_server()
+## serve_site()
+## stop_server()
+
+# Commit changes
+Files <- list.files(recursive = TRUE, full.names = TRUE)
+add(path = Files)
+commit(message = "Commit from git2r")
+## push() # TODO: Wise way to add credentials
 
 
-#### ###############################
-library("git2r")
+#### ############################### ###########################################
+
 
 site_repo <- "../../github/RKurs-VHS-2022"
 
